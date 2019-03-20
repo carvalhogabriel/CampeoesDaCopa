f05f7857e3ec90a1be71845b96fd75e01b770254
Gabriel Guerrero <gabriel.guerrero@venturus.org.br>
2019-03-20 11:10:07 -0300

    Init Project

---
+++ CampeosDaCopa/Controllers/WorldCupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WorldCupViewController.swift
+//  CampeosDaCopa
+//
+//  Created by Gabriel Carvalho Guerrero on 20/03/19.
+//  Copyright © 2019 Gabriel Carvalho Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class WorldCupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CampeosDaCopa/Models/WorldCup.swift
@@ -0,0 +1,9 @@
+//
+//  WorldCup.swift
+//  CampeosDaCopa
+//
+//  Created by Gabriel Carvalho Guerrero on 20/03/19.
+//  Copyright © 2019 Gabriel Carvalho Guerrero. All rights reserved.
+//
+
+import Foundation
